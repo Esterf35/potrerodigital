b68fcfdf2135daddb79a55c3f5fbd7d59992baf8
Esterf35 <104461428+Esterf35@users.noreply.github.com>
2022-06-29 23:48:16 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,17 @@
+
+
+function copy() {alert("by ESTER FERRARI")}
+
+function fondoblack(){
+
+        document.body.style.backgroundColor = "black";
+        document.getElementById("titulo1").style.color="white";
+        document.getElementById("titulo2").style.color="white";
+        document.getElementById("titulo_sec1").style.color = "white";
+        document.getElementById("titulo_sec2").style.color ="white" ;
+
+    }
+
+  function copy() {
+        alert("Autora:ESTER FERRARI. EMAIL: esterf24@gmail.com");
+        }
